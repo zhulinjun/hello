28df3f98617d91b2e704d46d6ede7cbfdca83387
zhu_lin_jun <zhu_lin_jun@163.com>
2014-09-20 16:53:25 +0800

    hello

---
+++ src/xx.c
@@ -0,0 +1,18 @@
+/*
+ * xx.c
+ *
+ *  Created on: 2014年9月19日
+ *      Author: zhulinjun
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include<string.h>
+
+int main(void) {
+	const char a='a';
+	printf("%c",a);
+
+
+	printf("ddd");
+	return 0;
+}
